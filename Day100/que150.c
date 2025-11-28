fd8fbf0605ae01262f65c90c9af4fd3f05998a50
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2025-11-28 22:18:06 +0530

    work

---
+++ Day100/que150.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    int marks;
+};
+
+int main() {
+    struct Student s;          
+    struct Student *ptr;       
+
+    ptr = &s;            
+
+    printf("Student pointer modifying values: ");
+    scanf("%s %d %d", ptr->name, &ptr->roll, &ptr->marks);
+
+    printf("Modified Data: Name: %s | Roll: %d | Marks: %d",
+           ptr->name, ptr->roll, ptr->marks);
+
+    return 0;
+}
